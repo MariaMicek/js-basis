9825767aabfae9386ddde9341b8d3d25ac74d3c3
Maria Micek <mmicek16@wp.pl>
2019-03-11 11:36:47 +0100

    mathematic oprations

--- script.js
+++ script.js
@@ -13,3 +13,26 @@ console.error(typeof myNumber);
 console.warn(typeof myBoolean);
 console.info(typeof myArray);
 console.debug(typeof myObject);
+
+var a = 5;
+a += 6;
+console.log(a);
+
+var b = 2;
+b += b // b = b + b
+b -+ b // b = b - b
+
+var c = 2;
+console.log(c--);   // c - 1 --> 2 najpierw wyświetla, potem odejmuje
+console.log(c);     // c = 1
+console.log(--c);   // c - 1 --> 0  najpierw odejmuje, potem wyświetla
+console.log(c);     // c = 0
+
+//Unary plus
+
++ 3     // 3
++ '3'   // 3
++ true  // 1
++ false // 0
++ null  // 0
++ {}    // NaN
