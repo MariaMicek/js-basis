6e2307cdbed927919ab37c6fb88088edd909f70e
Maria Micek <mmicek16@wp.pl>
2019-03-11 11:06:06 +0100

    typeof

---
+++ script.js
@@ -0,0 +1,15 @@
+var myString = "Hello";
+var myNumber = 16;
+var myBoolean = true;
+var myArray = ["Marysia", 38, false];
+var myObject = {a: "hello", b: "world", c: 21};
+
+console.log(myString, myNumber, myBoolean);
+console.log(myArray);
+console.log(myObject);
+
+console.log(typeof myString);
+console.log(typeof myNumber);
+console.log(typeof myBoolean);
+console.log(typeof myArray);
+console.log(typeof myObject);
