89dab634e6b4cf00f96b6e2178ab5ed160e080bc
Maria Micek <mmicek16@wp.pl>
2019-03-12 09:21:37 +0100

    Ternary operator exercise

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-var jestCieplo = false;
+// Make a variable, then make ternary operator that RETURNS: 1 if variable is equal to 1
+// 2 if variable is equal to "1"
+// that variable if if variable is equal to 2
+// 0 in other case
+// Console.log what was returned.
 
-var idziemyNaPiwo = jestCieplo ? 'Tak huurra' : 'Oh noo'
+var number = 39;
+var result = [1, 2, number, 0]
 
-console.log(idziemyNaPiwo);
+var numberCheck = (number === 1) ? result[0] :
+                  (number === '1') ? result[1] : 
+                  (number === 2 ) ? result[2] : 
+                  result[3] 
+
+console.log(numberCheck)
