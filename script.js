adf2b27aea0e2c243d681462441a392f015ed9f1
Maria Micek <mmicek16@wp.pl>
2019-03-12 13:43:48 +0100

    objects

--- script.js
+++ script.js
@@ -1,20 +1,48 @@
-var myArray = [1,2,3,4,5,6,7,8];
-var newArray = myArray.slice(2,6);
-console.log(newArray);
+var myCat = new Object();
 
-var array = [];
-var numbersLength = 12
+myCat.sound = "miau miau miau";
+myCat.sleep = "zzz";
+myCat.eat = "I am sleeping now";
+myCat.age = 2;
 
-for(i = 0; i < numbersLength; i++){
-    var myNumber = Math.round(Math.random() * 100);
-    array.push(myNumber);
+myCat.age = 3;
+myCat['age'] = 4;
+
+console.log(myCat);
+console.log(myCat.sound);
+console.log(myCat['sleep']);
+console.log(myCat.eat);
+console.log(myCat['age']);
+
+var myCar = {
+    speed: 70,
+    year: 2003,
+    color: 'red',
+    'color-2': 'green'
 }
 
-console.log(array);
+console.log(myCar);
+console.log(myCar['color-2']);
 
-var startIndex = numbersLength / 2 - 2;
-var endIndex = numbersLength / 2 + 2;
+myCar = {
+    mark: 'ford',
+    model: 'mustang',
+    year: '1997',
+    parts: {
 
-var newArray = array.slice(startIndex, endIndex)
+        engine: [1, 2, 3],
+        body: ['red', 'blue'],
 
-console.log(newArray)
+    },
+    sound: function() {
+        console.log('wrrrrr')
+    },
+    drive: function(speed) {
+        console.log('speed ' + speed);
+        console.log('speed', speed);
+    }
+}
+
+console.log(myCar.parts.body[1]);
+console.log(myCar.sound());
+console.log(myCar.drive(120));
