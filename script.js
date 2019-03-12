4daa2187c9c83b0f2f488224f1d974cac2284ffa
Maria Micek <mmicek16@wp.pl>
2019-03-12 14:01:48 +0100

    functions

--- script.js
+++ script.js
@@ -1,48 +1,30 @@
-var myCat = new Object();
-
-myCat.sound = "miau miau miau";
-myCat.sleep = "zzz";
-myCat.eat = "I am sleeping now";
-myCat.age = 2;
-
-myCat.age = 3;
-myCat['age'] = 4;
-
-console.log(myCat);
-console.log(myCat.sound);
-console.log(myCat['sleep']);
-console.log(myCat.eat);
-console.log(myCat['age']);
-
-var myCar = {
-    speed: 70,
-    year: 2003,
-    color: 'red',
-    'color-2': 'green'
+function f(x) {
+    
+    return 2 * x + 1;
 }
 
-console.log(myCar);
-console.log(myCar['color-2']);
+console.log(f(3) * 10);
+console.log(f(16));
+console.log(f(41));
 
-myCar = {
-    mark: 'ford',
-    model: 'mustang',
-    year: '1997',
-    parts: {
+var newValue = f(16) * 10;
+console.log(newValue);
 
-        engine: [1, 2, 3],
-        body: ['red', 'blue'],
+var newValue2 = f(f(14)) * f(6);
+console.log(f(newValue2));
 
-    },
-    sound: function() {
-        console.log('wrrrrr')
-    },
-    drive: function(speed) {
-        console.log('speed ' + speed);
-        console.log('speed', speed);
-    }
+function g(a, b, x) {
+    return a * x + b;
 }
 
-console.log(myCar.parts.body[1]);
-console.log(myCar.sound());
-console.log(myCar.drive(120));
+console.log(g(2, 3, 5));
+
+var c = 2;
+var d = 1;
+function h(x) {
+
+    return c * x + d;
+}
+
+console.log(h(3))
+
