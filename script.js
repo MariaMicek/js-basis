c20c48af27616f2a0cc45da34069233d8e767cbd
Maria Micek <mmicek16@wp.pl>
2019-03-12 10:02:36 +0100

    console.time/timeEnd - loops's time

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
-var numbers = [1,2,3,4,5]
+//tworzenie tablicy z milionem jedynek
 
-for (var i = 0; i < 4; i++){
-    console.log(numbers[i]);
+// var numbers = [];
+// for (var i = 0; i < 10000; i++){
+//     numbers.push(1);
+// }
+
+var numbers = [];
+for (var i = 0; i < 10000; i++){
+    numbers[i] = 1;
 }
 
+//czas z jakim jest wykonywana pętla
 
-var numbers = [5,6,7]
+console.time('in loop')
 
 for (var i = 0; i < numbers.length; i++){
     console.log(numbers[i]);
 }
 
+console.timeEnd ('in loop')
 
+console.time('before loop')
+
+var length = numbers.length
+for (var i = 0; i < length; i++){
+    console.log(numbers[i]);
+}
+
+console.timeEnd ('before loop')
