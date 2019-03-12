ef91ef382fa0a06949983a439d34bad531670091
Maria Micek <mmicek16@wp.pl>
2019-03-12 11:13:59 +0100

    continue, break

--- script.js
+++ script.js
@@ -1,40 +1,17 @@
-//do while
-var i = 5;
-
-do {
-    i += 1;
-    console.log(i);
-} while (i < 5); 
-
-
-//10 random numbers, not the same
+//continue and break
 var numbers = [];
 
 while (numbers.length < 10){
+
     var myNumber = Math.round((Math.random() * 100) + 1);
-    
-    var found = false;
-    for (var i = 1; i < numbers.length; i++){
-        if (myNumber == numbers[i]){
-            found = true;
-        }
+
+    if (!numbers.includes(myNumber)) {
+        if (myNumber > 20){
+        continue
+        // break
     }
 
-    if(!found) {
         numbers.push(myNumber);
-        console.log(myNumber)
-    }
-
-}
-
-//version 2
-numbers = [];
-
-while (numbers.length < 10){
-    var myNumber = Math.round((Math.random() * 100) + 1);
-    
-    if (!(myNumber in numbers)) {
-        numbers.push(myNumber);
-        console.log(myNumber)
+        console.log(myNumber);
     }
 }
