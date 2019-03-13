e5f483058380f48d5730f93c40a9d1710e634a03
Maria Micek <mmicek16@wp.pl>
2019-03-13 12:33:50 +0100

    assigning numbers to an empty array

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-//ARRAYS
-var myArray = [1,2,3,4,5];
+var myArray = new Array(10);
+for (var i = 0; i < myArray.length; i++) {
 
-console.log(myArray);
+   var myNumber = Math.ceil(Math.random() * 100);
+   myArray[i] = myNumber;
+   console.log(myArray[i]);
 
-myArray.push(6);
-myArray.unshift(0);
-
-console.log(myArray);
-
-myArray.pop();
-myArray.shift();
-
-console.log(myArray);
-
-
-var myArray2 = [1,2,3,4,5,6,7,8,9,10];
-console.log(myArray2.indexOf(9));   //8
-console.log(myArray2.lastIndexOf(5));   //4
-
-
-//OBJECTS
-var myCar = {
-
-    mark: 'Toyota',
-    year: 2000,
-    color: 'blue',
-    drive: function(){
-        console.log("Driving now")
-    }
 }
 
-console.log(myCar.mark);
-console.log(myCar.year);
-console.log(myCar.color);
-console.log(myCar.drive());
+
+var myArray3 = new Array(10);
+for (var i = 0; i < myArray3.length; i++) {
+
+    myArray3[i] = (i + 1)
+}
+console.log(myArray3);
 
 
+//not working
+// var myArray2 = new Array(10);
+// for (var i = 0; i < myArray2.length; i++) {
+
+//     myArray2.push(i + 1);
+    
+// }
+// console.log(myArray2);
