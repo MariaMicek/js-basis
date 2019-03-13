985619506a40d45a2e9bb9b73759d249a9613da0
Maria Micek <mmicek16@wp.pl>
2019-03-13 12:25:06 +0100

    arrays and objects exercises

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
-function f(x) {
-    
-    return 2 * x + 1;
+//ARRAYS
+var myArray = [1,2,3,4,5];
+
+console.log(myArray);
+
+myArray.push(6);
+myArray.unshift(0);
+
+console.log(myArray);
+
+myArray.pop();
+myArray.shift();
+
+console.log(myArray);
+
+
+var myArray2 = [1,2,3,4,5,6,7,8,9,10];
+console.log(myArray2.indexOf(9));   //8
+console.log(myArray2.lastIndexOf(5));   //4
+
+
+//OBJECTS
+var myCar = {
+
+    mark: 'Toyota',
+    year: 2000,
+    color: 'blue',
+    drive: function(){
+        console.log("Driving now")
+    }
 }
 
-console.log(f(3) * 10);
-console.log(f(16));
-console.log(f(41));
+console.log(myCar.mark);
+console.log(myCar.year);
+console.log(myCar.color);
+console.log(myCar.drive());
 
-var newValue = f(16) * 10;
-console.log(newValue);
-
-var newValue2 = f(f(14)) * f(6);
-console.log(f(newValue2));
-
-function g(a, b, x) {
-    return a * x + b;
-}
-
-console.log(g(2, 3, 5));
-
-var c = 2;
-var d = 1;
-function h(x) {
-
-    return c * x + d;
-}
-
-console.log(h(3))
 
