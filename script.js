e06adcf267084b42c06c7cdb41ec1b5b6177fad0
Maria Micek <mmicek16@wp.pl>
2019-03-16 15:30:30 +0100

    Return the Sum of the Two Smallest Numbers

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-var arr = [18, 48, -539, 60, -428, 3561, -2];
-var length = arr.length;
-var newArray = [];
-var y;
-var sum = 0;
-
-function getAbsSum() {
-
-    for (var i = 0; i < length; i++){
-
-        var x = arr[i];
-        
-        if (x < 0){
-                    y = x  - x - x;
-        } else {
-                    y = x;
-        }
-
-        newArray.push(y);
-        sum = sum + newArray[i]; 
-    }
-    return sum;
+function sumTwoSmallestNums(arr) {
+	var number = [];
+    var sum;
+	var position;
+	var min;
+	var min2;
+	for (i = 0; i < arr.length; i++){
+		
+		if (arr[i] > 0){
+			number.push(arr[i]);
+		}		
+	}
+		min = Math.min(...number); 
+		sum = min;					
+		position = number.indexOf(min);	
+		number.splice(position, 1);
+		min2 = Math.min(...number);
+		sum = sum + min2;
+		return sum;
+		
 }
 
-console.log(getAbsSum(arr))
+var arr = [-34,2,-9,-7,64,3,4,43,-5]
 
+console.log(sumTwoSmallestNums(arr))
